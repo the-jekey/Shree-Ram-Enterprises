e0f066b8ab76ff77f1df1ac19f30bfd4566abf62
the-jekey <jairamparihar79@gmail.com>
2025-10-14 22:18:34 +0530

    this is the last commit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -9,7 +9,7 @@ import TextReveal from './TextReveal';
 const Main = (props) => {
 
        return (
-              <div data-scroll data-scroll-speed="" className='h-screen relative  overflow-hidden lg:h-screen max-w-screen mx-auto bg-[url("")] bg-cover bg-center bg-no-repeat  '>
+              <div data-scroll data-scroll-speed="" className='h-[91vh] relative  overflow-hidden lg:h-screen max-w-screen mx-auto bg-[url("")] bg-cover bg-center bg-no-repeat  '>
                      <div id='Home' className="relative w-full h-screen">
 
                             <GrainyFilter />
